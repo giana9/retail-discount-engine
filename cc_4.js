9d91c1ad35a5225125dd978d8eae50635881a674
Giana Cresta <crestagiana@gmail.com>
2026-06-05 16:44:59 -0400

    product array

---
+++ cc_4.js
@@ -0,0 +1,33 @@
+// Product Array
+let products = [
+    {
+        name: "tablet",
+        category: "electronics",
+        price: 500,
+        inventoryCount: 10
+    }, 
+    {
+        name: "jeans",
+        category: "apparel",
+        price: 50,
+        inventoryCount: 25
+    },
+    {
+        name: "blueberries",
+        category: "groceries",
+        price: 5,
+        inventoryCount: 30
+    },
+    {
+        name: "lamp",
+        category: "household",
+        price: 20,
+        inventoryCount: 15
+    },
+    {
+        name: "dog food",
+        category: "pet",
+        price: 30,
+        inventoryCount: 13
+    }
+];
